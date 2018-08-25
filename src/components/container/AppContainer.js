25673a5289b40109457bb8aafeb71b464c1a9f0b
Maximilian König <koenig.maximilian@outlook.com>
2018-08-25 21:24:45 +0200

    Merge fix 2

--- src/components/container/AppContainer.js
+++ src/components/container/AppContainer.js
@@ -23,14 +23,9 @@ export default class AppContainer extends Component {
               classNames="fade"
             >
               <Switch location={location}>
-<<<<<<< HEAD
-                <Route exact path="/asd" component={DashboardContainer} />
-                <Route exact path="/" component={DonationContainer} />
-=======
                 <Route exact path="/" component={DashboardContainer} />
                 <Route exact path="/donate" component={DonationContainer} />
                 <Route exact path="/profile" component={ProfileContainer} />
->>>>>>> 0dddcc1d990fe00577dd997f5eed910d40c11e92
               </Switch>
             </CSSTransition>
           </TransitionGroup>
