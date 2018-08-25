94351fa36c7391525604c41c31a888a3e961947e
albin <me@albxn.com>
2018-08-26 00:16:09 +0200

    dashboard in components

--- src/components/container/DashboardContainer.js
+++ src/components/container/DashboardContainer.js
@@ -1,118 +1,42 @@
 import React, { Component } from "react";
-//import {DonateButton} from "../display/DonateButton";
+import dashboardData from '../../data/dashboardData.json';
+import {TileRowLarge} from "./TileRowLarge";
+import {TileRow} from "./TileRow";
 
 export class DashboardContainer extends Component {
     constructor(){
         super();
+        this.state = {
+        	"projectsAndStories": dashboardData.projectsAndStories,
+        	"projectsAndCategories": dashboardData.projectsAndCategories,
+        	"myFavorites" : dashboardData.myFavorites
+        }
     }
+
+
 
     render(){
         return (
         	<div className="dashboard-wrapper">
-        		<div className="row">
-        			<div className="row-title font-bold">Aktuelle Stories und Projekte</div>
-        			<div className="row-wrapper">
-	        			<div className="row-scroller" style={{ width : '915px' }}>
-	        				
-	        				<div className="tile-lg" style={{ backgroundImage : "url(./src/img/kategorien/opfer_vergessener_katastrophen.jpg)"}}>
-	        					<div className="tile-tag-live">
-	        						<div className="dot"></div>
-	        						LIVE
-	        					</div>
-	        					<div className="tile-text font-light">
-	        						Jeder zweite Syrer ist auf der Flucht, etwa die Hälfte der Betroffenen sind Kinder. 
-	        					</div>
-	        				</div>
 
-	        				<div className="tile-lg" style={{ backgroundImage : "url(./src/img/kategorien/das_ambulatorium_fuer_folter_und_kriegsopfer.jpg)"}}>
-	        					<div className="tile-tag">STORY</div>
-	        					<div className="tile-text font-light">
-	        						Jeder zweite Syrer ist auf der Flucht, etwa die Hälfte der Betroffenen sind Kinder. 
-	        					</div>
-	        				</div>
+        		<TileRowLarge
+        			title="Aktuelle Projekte und Stories"
+        			size="915"
+        			form="large"
+        			json={this.state.projectsAndStories}
+        		/>
 
-	        				<div className="tile-lg" style={{ backgroundImage : "url(./src/img/kategorien/fluechtlinge_in_asien_brauchen_hilfe.jpg)"}}>
-	        					<div className="tile-tag">STORY</div>
-	        					<div className="tile-text font-light">
-	        						Jeder zweite Syrer ist auf der Flucht, etwa die Hälfte der Betroffenen sind Kinder. 
-	        					</div>
-	        				</div>
+        		<TileRow
+        			title="Dafür setzt sich das SRK ein"
+        			size="900"
+        			json={this.state.projectsAndCategories}
+        		/>
 
-	        				<br style={{clear:'left'}} />
-	        			</div>
-	        		</div>
-        		</div>
-
-        		<div className="row">
-        			<div className="row-title font-bold">Dafür setzt sich das SRK ein</div>
-        			<div className="row-wrapper">
-	        			<div className="row-scroller" style={{ width : '900px' }}> 
-	        				
-	        				<div className="tile" style={{ backgroundImage : "url(./src/img/kategorien/fluechtlinge_in_asien_brauchen_hilfe.jpg)"}}>
-	        					<div className="tile-text font-light">
-		        					Flüchtlinge in Asien brauchen Hilfe.
-		        				</div>
-	        				</div>
-
-	        				<div className="tile" style={{ backgroundImage : "url(./src/img/kategorien/hilfe_fuer_familien_in_der_schweiz.jpg)"}}>
-	        					<div className="tile-text font-light">
-	        						Hilfe für Familien in der Schweiz.
-	        					</div>
-	        				</div>
-
-	        				<div className="tile" style={{ backgroundImage : "url(./src/img/kategorien/hunger_in_afrika.jpg)"}}>
-	        					<div className="tile-text font-light">
-	        						Afrika hat Hunger.
-	        					</div>
-	        				</div>
-
-	        				<div className="tile" style={{ backgroundImage : "url(./src/img/kategorien/licht_in_das_leben_der_aermsten_bringen.jpg)"}}>
-	        					<div className="tile-text font-light">
-	        						Licht in das Leben der Ärmsten bringen.
-	        					</div>
-	        				</div>
-
-	        				<br style={{clear:'left'}} />
-	        			</div>
-	        		</div>
-        		</div>
-
-        		<div className="row">
-        			<div className="row-title font-bold">Meine Favoriten</div>
-        			<div className="row-wrapper">
-	        			<div className="row-scroller" style={{ width : '900px' }}> 
-	        				
-	        				<div className="tile" style={{ backgroundImage : "url(./src/img/kategorien/fluechtlinge_in_asien_brauchen_hilfe.jpg)"}}>
-	        					<div className="tile-heart"></div>
-	        					<div className="tile-text font-light">
-		        					Flüchtlinge in Asien brauchen Hilfe.
-		        				</div>
-	        				</div>
-
-	        				<div className="tile" style={{ backgroundImage : "url(./src/img/kategorien/hilfe_fuer_familien_in_der_schweiz.jpg)"}}>
-	        					<div className="tile-heart"></div>
-	        					<div className="tile-text font-light">
-	        						Hilfe für Familien in der Schweiz.
-	        					</div>
-	        				</div>
-
-	        				<div className="tile" style={{ backgroundImage : "url(./src/img/kategorien/hunger_in_afrika.jpg)"}}>
-	        					<div className="tile-heart"></div>
-	        					<div className="tile-text font-light">
-	        						Afrika hat Hunger.
-	        					</div>
-	        				</div>
-
-	        				<div className="tile" style={{ backgroundImage : "url(./src/img/kategorien/licht_in_das_leben_der_aermsten_bringen.jpg)"}}>
-	        					<div className="tile-text font-light">
-	        						Licht in das Leben der Ärmsten bringen.
-	        					</div>
-	        				</div>
-
-	        				<br style={{clear:'left'}} />
-	        			</div>
-	        		</div>
-        		</div>
+        		<TileRow
+        			title="Meine Favoriten"
+        			size="900"
+        			json={this.state.myFavorites}
+        		/>
 
         	</div>
         );
---
+++ src/components/container/TileRow.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import {Tile} from "../display/Tile";
+
+export class TileRow extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+            <div className="row">
+              <div className="row-title font-bold">{this.props.title}</div>
+              <div className="row-wrapper">
+                <div className="row-scroller" style={{ width : this.props.size+"px" }}>
+                {
+                	this.props.json.map((val, key) => {
+                		return (
+                			<Tile
+                			data={{
+                				text : val.title,
+                				image : val.image,
+                        heart : val.heart
+                			}}
+                			/>
+                		)
+                	})
+                }
+                  <br style={{clear:'left'}} />
+                
+                
+                </div>
+              </div>
+            </div>
+    );
+  }
+}
