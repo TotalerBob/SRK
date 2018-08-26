96910aa3435c48ef5cdf84e527ceeb3d6eb52c22
precyx <pr.pascal.bachmann@gmail.com>
2018-08-26 04:26:18 +0200

    add badges new

--- src/components/container/ProfileContainer.js
+++ src/components/container/ProfileContainer.js
@@ -33,21 +33,18 @@ export class ProfileContainer extends Component {
           </div>
           <ProfileButton />
 
+          <div class="silver-line"></div>
+
           <h2>Meine Errungenschaften</h2>
-          <div class="badges">
-            <div class="badge">
-              <img src="src/img/badge_1.svg" alt="" />
-              <div class="description">Erste Spende</div>
-            </div>
-            <div class="badge">
-              <img src="src/img/badge_2.svg" alt="" />
-              <div class="description">In 5 Ländern gespendet</div>
-            </div>
-            <div class="badge">
-              <img src="src/img/badge_3.svg" alt="" />
-              <div class="description">Für 10 Projekte gespendet</div>
-            </div>
+          <div class="profile-badges">
+            <img src="src/img/badges/badges.png" alt=""/>
           </div>
+          <div class="silver-line"></div>
+          <div class="profile-stats">
+            <div class="line"><div class="name">Insgesamt gespendet:</div> <div class="val">CHF 3'520.-</div></div>
+            <div class="line"><div class="name">Anzahl Projekte gespendet: </div> <div class="val">14</div> </div>
+          </div>
+
         </div>
       </div>
     );
